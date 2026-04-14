a237247d9c740916a1a531c32f95069f6170a5ed
nazarethbran <nazarethbran83@gmail.com>
2026-04-14 00:13:11 -0600

    Estructura inicial

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Ruta de prueba
+app.get('/', (req, res) => {
+    res.send("API funcionando 🚀");
+});
+
+app.listen(3000, () => {
+    console.log("Servidor en puerto 3000");
+});
